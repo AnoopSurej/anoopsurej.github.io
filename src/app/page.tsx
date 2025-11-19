8ba2b39be78e8051e17a7c1f7bbd1ec960f72928
Anoop Surej <surejanoop@gmail.com>
2025-11-19 21:24:02 +0000

    feat: Add initial nextjs files

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1>
+          Anoop's Portfolio!
+        </h1>
+      </main>
+    </div>
+  );
+}
